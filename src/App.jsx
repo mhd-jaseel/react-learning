c8b5f99c6be6bff39178815565bc3d535a09aee7
mhd-jaseel <mohammejaseel90@gmail.com>
2026-03-31 14:46:20 +0530

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,47 @@
 // import Counter from "./component/Counter";
 // import Form from "./component/Form";
 // import DeleteList from "./component/DeleteList";
-import MiniApp from "./component/MiniApp";
-function App(){
-  return(
+// import MiniApp from "./component/MiniApp/MiniApp";
+// function App(){
+//   return(
+//     <div>
+//       {/* <Toggle/>
+//       <Counter/>
+//        <Form/>
+//        <DeleteList/> */}
+//        <MiniApp/>
+//     </div>
+//   )
+// }
+
+              //use state
+// import React from 'react'
+// import Name from './component/useState';
+
+//  function App() {
+//   return (
+//     <div>
+//       <Name/>
+
+//     </div>
+//   )
+// }
+//  export default App;
+
+       //useEffect
+
+import React from 'react'
+import Example from './component/useEffect/withEmpty'
+import NoDependency from './component/useEffect/noDependency'
+import DependencyValue from './component/useEffect/dependencyValue'
+
+export default function App() {
+  return (
     <div>
-      {/* <Toggle/>
-      <Counter/>
-       <Form/>
-       <DeleteList/> */}
-       <MiniApp/>
+      <NoDependency/>
+      <Example/>
+      <DependencyValue/>
+
     </div>
   )
 }
- export default App;
